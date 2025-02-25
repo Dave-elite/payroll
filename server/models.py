23d29f07b4b2383193a9071c4c00fea0721197a2
Dave-elite <davenjenga098@gmail.com>
2025-02-25 10:13:23 +0300

    Imporoved the user models id

--- server/models.py
+++ server/models.py
@@ -16,7 +16,7 @@ class User(db.Model, SerializerMixin):
     """
     __tablename__ = 'users'
     
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(10000), nullable=False)
