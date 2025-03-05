3585c9c07c11a8e03a233f98d27f61882cbfe10f
Dave-elite <davenjenga098@gmail.com>
2025-03-05 16:28:33 +0300

    Updated the database

--- server/migrations/versions/40d376db6d47_initial_migration.py
+++ server/migrations/versions/40d376db6d47_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration
 
-Revision ID: 9534fbfd44a5
+Revision ID: 40d376db6d47
 Revises: 
-Create Date: 2025-02-18 15:55:37.164785
+Create Date: 2025-03-05 15:35:24.346163
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9534fbfd44a5'
+revision = '40d376db6d47'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -103,14 +103,14 @@ def upgrade():
     sa.PrimaryKeyConstraint('tax_id')
     )
     op.create_table('users',
-    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('user_id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(length=100), nullable=False),
     sa.Column('email', sa.String(length=255), nullable=False),
-    sa.Column('password_hash', sa.String(length=10000), nullable=False),
+    sa.Column('password', sa.String(length=10000), nullable=False),
     sa.Column('role', sa.String(length=255), nullable=False),
     sa.Column('employee_id', sa.Integer(), nullable=True),
     sa.ForeignKeyConstraint(['employee_id'], ['employees.employee_id'], ),
-    sa.PrimaryKeyConstraint('id'),
+    sa.PrimaryKeyConstraint('user_id'),
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('employee_id')
     )
