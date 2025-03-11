71ca768a33cf2895b0428f2ada0f946356c6b3a6
Dave-elite <davenjenga098@gmail.com>
2025-03-11 15:41:31 +0300

    updated the models.py

--- server/models.py
+++ server/models.py
@@ -157,7 +157,7 @@ class Attendance(db.Model, SerializerMixin):
     employee_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'), nullable=False)
     date = db.Column(db.Date(), nullable=False)
     clock_in_time = db.Column(db.String(100), nullable=False)
-    clock_out_time = db.Column(db.String(100), nullable=False)
+    clock_out_time = db.Column(db.String(100), nullable=True)
     status = db.Column(db.String(100), nullable=False)
     
     # Relationship with Employee
